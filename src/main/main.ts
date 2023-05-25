7d0827e142d47a42fe46593aa453f8d89fb83ae9
Kyle <kyle.w@binance.com>
2023-05-25 22:32:39 +0300

    init

--- src/main/main.ts
+++ src/main/main.ts
@@ -81,7 +81,9 @@ const createWindow = async () => {
     },
   });
 
-  mainWindow.loadURL(resolveHtmlPath('index.html'));
+  mainWindow.loadURL('https://videocall.agora.io/room?rid=SKIBTG', {
+    userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+  });
 
   mainWindow.on('ready-to-show', () => {
     if (!mainWindow) {
